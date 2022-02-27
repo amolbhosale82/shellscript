e11225ddb9ef89fc573f0baa6e09300325763fd1
Amol Bhosale <amolbhosale82@gmail.com>
2022-02-26 23:33:25 -0700

    update

--- mathexpr.sh
+++ mathexpr.sh
@@ -3,6 +3,6 @@ echo "Enter 1st number"
 read num1
 echo "Enter 2nd number"
 read num2
-echo "Subtraction"
-echo "$num1 - $num2"
-echo `expr $num1 - $num2`
+echo "Division"
+echo "$num1 / $num2"
+echo `expr $num1 / $num2`
