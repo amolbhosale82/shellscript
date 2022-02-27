efe48676d3d6e49ceff89c91117d9e1f8ce31372
Amol Bhosale <amolbhosale82@gmail.com>
2022-02-27 00:13:07 -0700

    modified

--- if-condition.sh
+++ if-condition.sh
@@ -4,6 +4,12 @@ read num
 if [ $num -lt 10 ]
 then
     echo "This is Single digit number."
+elif [ $num -lt 100 ]
+then 
+    echo "This is a double digit number."
+elif [ $num -lt 1000 ]
+then
+    echo "This is three digit number."
 else
-    echo "This is not a Single digit number"
+    echo "This is more than three digit number"
 fi
